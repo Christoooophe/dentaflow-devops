d99c5e6423292c745eddf7b5b697f68e9c1ea99d
Christoooophe <christophealfier02@gmail.com>
2026-01-28 15:25:00 +0100

    :construction_worker: test: Premier test du pipeline CI/CD

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+const { js } = require("@eslint/js")
+const { globals } = require("globals")
+const { defineConfig } = require("eslint/config");
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
